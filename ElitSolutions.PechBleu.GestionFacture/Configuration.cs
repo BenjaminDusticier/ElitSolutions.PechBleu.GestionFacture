39c049574289769905e3d15bdb56aa03617ac5d0
BenjaminDusticier <62941994+BenjaminDusticier@users.noreply.github.com>
2022-04-15 15:09:27 +0200

    zob

--- ElitSolutions.PechBleu.GestionFacture/Configuration.cs
+++ ElitSolutions.PechBleu.GestionFacture/Configuration.cs
@@ -1,3 +1,4 @@
+using MFiles.VAF.Common;
 using MFiles.VAF.Configuration;
 using MFiles.VAF.Configuration.JsonAdaptor;
 using System;
@@ -9,5 +10,63 @@ namespace ElitSolutions.PechBleu.GestionFacture
     [DataContract]
     public class Configuration
     {
+        /// <summary>
+        /// A <see cref="MFIdentifier"/> for the class
+        /// with alias "MF.Class.Fou".
+        /// </summary>
+        /// <remarks>If this alias cannot be resolved then the ID will be -1.</remarks>
+        [MFClass(Required = true)]
+        [DataMember]
+        [JsonConfEditor(Label = "Classe des fournisseurs")]
+        public MFIdentifier ClassFournisseur { get; set; }
+            = "MFiles.Class.Fournisseur";
+
+
+        /// <summary>
+        /// A <see cref="MFIdentifier"/> for the property definition
+        /// with alias "MF.PD.NumFou".
+        /// </summary>
+        /// <remarks>If this alias cannot be resolved then the ID will be -1.</remarks>
+        [MFPropertyDef(Required = true)]
+        [DataMember]
+        [JsonConfEditor(Label = "Numéro du fournisseur")]
+        public MFIdentifier PropDefNumFou { get; set; }
+            = "MFiles.PropertyDef.NumFou";
+
+        /// <summary>
+        /// A <see cref="MFIdentifier"/> for the class
+        /// with alias "MF.Class.Fac".
+        /// </summary>
+        /// <remarks>If this alias cannot be resolved then the ID will be -1.</remarks>
+        [MFClass(Required = true)]
+        [DataMember]
+        [JsonConfEditor (Label = "Classe des factures")]
+        public MFIdentifier ClassFacture { get; set; }
+            = "MFiles.Class.Facture";
+
+
+        /// <summary>
+        /// A <see cref="MFIdentifier"/> for the property definition
+        /// with alias "MF.PD.Fou".
+        /// </summary>
+        /// <remarks>If this alias cannot be resolved then the ID will be -1.</remarks>
+        [MFPropertyDef(Required = true)]
+        [DataMember]
+        [JsonConfEditor(Label = "Fournisseur des factures")]
+        public MFIdentifier PropDefFournisseur { get; set; }
+            = "MFiles.PropertyDef.Fournisseur";
+
+
+        /// <summary>
+        /// A <see cref="MFIdentifier"/> for the property definition
+        /// with alias "MF.PD.NumFac".
+        /// </summary>
+        /// <remarks>If this alias cannot be resolved then the ID will be -1.</remarks>
+        [MFPropertyDef(Required = true)]
+        [DataMember]
+        [JsonConfEditor(Label = "Numéro de facture")]
+        public MFIdentifier PropDefNumFac { get; set; }
+            = "MF.PD.NumFac";
+
     }
 }
--- ElitSolutions.PechBleu.GestionFacture/VaultApplication.cs
+++ ElitSolutions.PechBleu.GestionFacture/VaultApplication.cs
@@ -6,6 +6,7 @@ using MFiles.VAF.Core;
 using MFilesAPI;
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace ElitSolutions.PechBleu.GestionFacture
 {
@@ -16,8 +17,6 @@ namespace ElitSolutions.PechBleu.GestionFacture
     public class VaultApplication
         : ConfigurableVaultApplicationBase<Configuration>
     {
-
-
         /// <summary>
         /// Executed when an object is moved into a workflow state
         /// with alias "WFS.WfTest.001".
@@ -26,25 +25,96 @@ namespace ElitSolutions.PechBleu.GestionFacture
         [StateAction("WFS.WfTest.001")]
         public void MyWorkflowStateHandler(StateEnvironment env)
         {
-            // Récupère le filever de l'objet.
-            ObjectFiles objfiles = env.Vault.ObjectFileOperations.GetFiles(env.ObjVer);
-            ObjectFile objfile = objfiles[1];
-            FileVer filever = objfile.FileVer;
-
-            // Récupère le contenu texte.
-            env.Vault.ObjectFileOperations.GetTextContentForFile(env.ObjVer, filever);
 
             //
-            int numlot = -1;
+            SysUtils.ReportInfoToEventLog("Toto");
+            SysUtils.ReportToEventLog("Tata", EventLogEntryType.Warning);
+
+            // Récupère le filever de l'objet.
+            ObjectFiles objFiles = env.Vault.ObjectFileOperations.GetFiles(env.ObjVer);
+            ObjectFile objFile = objFiles[0];
+            FileVer fileVer = objFile.FileVer;
+
+            // Récupère le contenu texte.
+            string contenu = env.Vault.ObjectFileOperations.GetTextContentForFile(env.ObjVer, fileVer);
+
+            // Copie les lignes du contenu dans un tableau .
+            string[] lignes = contenu.Split('\n');
+
+            //
+            int numFac;
+            int numTiers;
+            int numLot;
+            int numLotPrec = -1;
+
+            //
+            string ec = "";
 
             // Pour chaque ligne du contenu texte.
-            foreach (var item in collection)
+            foreach ( string ligne in lignes )
             {
+                // Enregistre la ligne.
+                ec += ligne + '\n';
 
+                // Copie les chaînes séparées par tabulation dans un tableau.
+                string[] valeurs = ligne.Split('\t');
+
+                // Récupère le numéro de facture.
+                numFac = int.Parse(valeurs[3]);
+
+                // Récupère le numéro de tiers.
+                numTiers = int.Parse(valeurs[6]);
+
+                // Récupère le numéro de lot.
+                numLot = int.Parse(valeurs[0]);
+
+                // Passe à la ligne suivante si le numéro de lot a changé.
+                if ( numLotPrec == numLot ) { continue; }
+
+                // Mémorise le numéro de lot.
+                numLotPrec = numLot;
+
+                // Cherche le fournisseur.
+                var searchBuilder = new MFSearchBuilder(env.Vault);
+                searchBuilder.Class(this.Configuration.ClassFournisseur);
+                searchBuilder.Property(this.Configuration.PropDefNumFou, MFDataType.MFDatatypeText, numTiers);
+                //// Only items with an external ID (display ID) of "SUP12345".
+                //searchBuilder.Status(MFStatusType.MFStatusTypeExtID, MFDataType.MFDataTypeText, "SUP12345")
+                searchBuilder.Deleted(false);
+                var searchResults = searchBuilder.FindEx();
+
+                // Passe à la ligne suivante si aucun résultat.
+                if ( searchResults.Count <= 0) { continue; }
+                    
+                // Récupère l'ID du fournisseur trouvé.
+                int idFou = searchResults[0].ID;
+
+                // Cherche la facture.
+                searchBuilder = new MFSearchBuilder(env.Vault);
+                searchBuilder.Class(this.Configuration.ClassFacture);
+                searchBuilder.Property(this.Configuration.PropDefFournisseur, MFDataType.MFDatatypeLookup, idFou);
+                searchBuilder.Property(this.Configuration.PropDefNumFac, MFDataType.MFDatatypeText, numFac);
+                searchBuilder.Deleted(false);
+                searchResults = searchBuilder.FindEx();
+
+                // Passe à la ligne suivante si aucun résultat.
+                if (searchResults.Count <= 0) { continue; }
+
+                //// Crée le fichier temporaire.
+                //string tempFileName = Path.GetTempFileName();
+
+                //// Ajoute les écritures au fichier temporaire.
+                //StreamWriter streamWriter = File.AppendText(tempFileName);
+                //streamWriter.Write(ec);
+                //streamWriter.Flush();
+                //streamWriter.Close();
+
+                //// Crée le document.
+
+                //// Supprime le fichier temporaire s'il existe.
+                //if ( File.Exists(tempFileName) ) { File.Delete(tempFileName); }
             }
-
             //throw new System.NotImplementedException();
         }
-
     }
 }
